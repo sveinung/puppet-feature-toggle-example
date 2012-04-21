edea31eeaddeb1ee9e678b69d7601a0271c23771
Sveinung Dalatun <sdalatun@gmail.com>
2012-04-21 20:48:31 +0200

    Added a toggle for feature one to host1 set to 'true'.

--- puppet/manifests/host1.pp
+++ puppet/manifests/host1.pp
@@ -2,3 +2,12 @@ include hosts
 
 import 'default.pp'
 
+$feature_toggle_file = 'toggle_example'
+
+toggle { $feature_toggle_file: }
+
+toggle::fragment { 'one':
+  target  => $feature_toggle_file,
+  setting => true,
+}
+
