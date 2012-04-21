852bfc362e5a62088577c3244b2ed36407c95755
Sveinung Dalatun <sdalatun@gmail.com>
2012-04-21 20:17:52 +0200

    Added package resources: sinatra, g++, sinatra-contrib and haml.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,3 +1,22 @@
 group { 'puppet':
   ensure => present,
 }
+
+Package {
+  ensure => present,
+}
+
+package { 'sinatra':
+  provider => "gem",
+}
+
+package { 'g++': }
+
+package { 'sinatra-contrib':
+  provider => "gem",
+  require  => Package['g++'],
+}
+
+package { 'haml':
+  provider => "gem",
+}
