4d1d5576a84b450860b9d120f6e566cae68ed6e1
Sveinung Dalatun <sdalatun@gmail.com>
2012-04-29 19:29:20 +0200

    Added thin resource starting the example application.

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -2,6 +2,12 @@ $feature_toggle_file = 'toggle_example'
 
 node base {
   import 'default.pp'
+  include thin
+
+  thin::app { 'example':
+    appdir => '/vagrant',
+    port   => 8080,
+  }
 
   config { $feature_toggle_file: }
 }
