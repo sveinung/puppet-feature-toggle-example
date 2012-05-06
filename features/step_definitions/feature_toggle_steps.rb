101f1cc2c6b4f6ed0a5a3eaef132e1446156fd3a
Sveinung Dalatun <sdalatun@gmail.com>
2012-05-06 10:51:48 +0200

    Referenced non-existing variable text.

--- features/step_definitions/feature_toggle_steps.rb
+++ features/step_definitions/feature_toggle_steps.rb
@@ -6,6 +6,6 @@ Then /^I should see "([^"]*)"$/ do |text|
   page.should have_content(text)
 end
 
-Then /^I should NOT see "([^"]*)"$/ do |arg1|
+Then /^I should NOT see "([^"]*)"$/ do |text|
   page.should_not have_content(text)
 end
